8967e7ded2cbc4074bf4b4ec5af817adfd79b77e
Connor <homes.hammer-73@icloud.com>
2020-07-10 04:18:13 -0400

    Add Thumb register compare negative

--- src/thumb_exec.zig
+++ src/thumb_exec.zig
@@ -411,6 +411,22 @@ pub fn buildThumbTraceAt(word: u16, pc: u32, tape: *trace.Tape) RunError!void {
         return;
     }
 
+    if ((word & 0xffc0) == 0x42c0) {
+        const source_reg = try tape.literalReg(arm_state.lowReg(word >> 3));
+        const dest = try tape.literalReg(arm_state.lowReg(word));
+        const carry_in = try tape.literalBit(false);
+        const left = try tape.loadReg(dest);
+        const right = try tape.loadReg(source_reg);
+        const result = try tape.addCarrying(left, right, carry_in);
+        const carry_out = try tape.carryResult(result);
+        const overflow = try tape.overflowResult(result);
+        _ = try tape.storeNegative(try tape.highBit(result));
+        _ = try tape.storeZero(try tape.equalZero(result));
+        _ = try tape.storeCarry(carry_out);
+        _ = try tape.storeOverflow(overflow);
+        return;
+    }
+
     if ((word & 0xff00) == 0x4400) {
         const dest_reg = arm_state.reg4(((word >> 4) & 8) | (word & 7));
         const addend_reg = arm_state.reg4(word >> 3);
@@ -667,6 +683,16 @@ pub fn runThumb(word: u16, state: *arm_state.MachineState) RunError!void {
         const source = arm_state.lowReg(word >> 3);
         const dest = arm_state.lowReg(word);
         const result = subWithCarry(state.read(dest), state.read(source), true);
+        updateNz(state, result.word);
+        state.setCarry(result.carry);
+        state.setOverflow(result.overflow);
+        return;
+    }
+
+    if ((word & 0xffc0) == 0x42c0) {
+        const source = arm_state.lowReg(word >> 3);
+        const dest = arm_state.lowReg(word);
+        const result = addWithCarry(state.read(dest), state.read(source), false);
         updateNz(state, result.word);
         state.setCarry(result.carry);
         state.setOverflow(result.overflow);
