0bb6ee26137ceaec3a36e3a4205329d859d57399
Connor <homes.hammer-73@icloud.com>
2020-08-07 20:36:17 -0400

    Flatten module layout

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("jit-core", "src/jit_core.zig");
+    const lib = b.addStaticLibrary("jit-core", "jit_core.zig");
     lib.setBuildMode(mode);
     lib.install();
 
