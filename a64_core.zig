405a931a3c8d5f1b4e37918a421944f3a929258f
Connor <homes.hammer-73@icloud.com>
2021-12-08 13:23:03 -0500

    Implement 64-bit unprivileged loads

--- a64_core.zig
+++ a64_core.zig
@@ -417,6 +417,10 @@ pub const Core64 = struct {
             return try self.runVectorLoadStoreRegister(word, offset, false, false);
         }
 
+        if ((word & 0x3f200c00) == 0x38000800) {
+            return try self.runLoadStoreUnprivileged(word);
+        }
+
         if ((word & 0x3f200c00) == 0x38000000) {
             const offset = @bitCast(u64, bits.signExtend64(@as(u64, (word >> 12) & 0x1ff), 9));
             return try self.runLoadStoreRegister(word, offset, false, false);
@@ -481,6 +485,35 @@ pub const Core64 = struct {
             }
             self.writeSized(true, base_reg, address, true);
         }
+        self.state.pc +%= 4;
+        return true;
+    }
+
+    fn runLoadStoreUnprivileged(self: *Core64, word: u32) Core64Error!bool {
+        const size = @intCast(u2, word >> 30);
+        const opcode = @intCast(u2, (word >> 22) & 3);
+        const base_reg = regFromWord(word >> 5);
+        const data_reg = regFromWord(word);
+        const bytes = @as(usize, 1) << size;
+        const address = self.readSized(true, base_reg, true) +% @as(u64, (word >> 12) & 0x1ff);
+
+        if ((opcode & 2) == 0) {
+            if ((opcode & 1) == 0) {
+                try self.writeMemory(address, bytes, self.readSized(size == 3, data_reg, false));
+            } else {
+                const value = try self.readMemory(address, bytes);
+                self.writeSized(size == 3, data_reg, value, false);
+            }
+        } else {
+            if (size == 3 or (size == 2 and (opcode & 1) != 0)) {
+                return error.UnallocatedEncoding;
+            }
+
+            const value = try self.readMemory(address, bytes);
+            const extended = @bitCast(u64, bits.signExtend64(value, @intCast(u6, bytes * 8)));
+            self.writeSized((opcode & 1) == 0, data_reg, extended, false);
+        }
+
         self.state.pc +%= 4;
         return true;
     }
