c6c2ee260a9d3ed196e1b6ed3575aae9a13a3999
Connor <homes.hammer-73@icloud.com>
2021-12-23 05:42:19 -0500

    Implement 64-bit vector structure transfers

--- a64_core.zig
+++ a64_core.zig
@@ -162,6 +162,13 @@ pub const Core64 = struct {
             if (vector_pair_load_store) {
                 return;
             }
+            const vector_structure_transfer = self.runVectorStructureTransfer(word) catch |err| {
+                try self.raiseFault(err);
+                return;
+            };
+            if (vector_structure_transfer) {
+                return;
+            }
             const logical_immediate = self.runLogicalImmediate(word) catch |err| {
                 try self.raiseFault(err);
                 return;
@@ -703,6 +710,101 @@ pub const Core64 = struct {
             }
             self.writeSized(true, base_reg, address, true);
         }
+        self.state.pc +%= 4;
+        return true;
+    }
+
+    fn runVectorStructureTransfer(self: *Core64, word: u32) Core64Error!bool {
+        if ((word & 0x3f000000) != 0x0c000000 or (word & 0x00200000) != 0) {
+            return false;
+        }
+
+        const writeback = (word & 0x00800000) != 0;
+        const load = (word & 0x00400000) != 0;
+        const offset_reg_bits = (word >> 16) & 0x1f;
+        if (!writeback and offset_reg_bits != 0) {
+            return false;
+        }
+
+        const op = @intCast(u4, (word >> 12) & 0xf);
+        var repeat: usize = undefined;
+        var fields: usize = undefined;
+        switch (op) {
+            0x0 => {
+                repeat = 1;
+                fields = 4;
+            },
+            0x2 => {
+                repeat = 4;
+                fields = 1;
+            },
+            0x4 => {
+                repeat = 1;
+                fields = 3;
+            },
+            0x6 => {
+                repeat = 3;
+                fields = 1;
+            },
+            0x7 => {
+                repeat = 1;
+                fields = 1;
+            },
+            0x8 => {
+                repeat = 1;
+                fields = 2;
+            },
+            0xa => {
+                repeat = 2;
+                fields = 1;
+            },
+            else => return error.UnallocatedEncoding,
+        }
+
+        const full = (word & 0x40000000) != 0;
+        const lane_bytes = @as(usize, 1) << @intCast(u3, (word >> 10) & 3);
+        if (lane_bytes == 8 and !full and fields != 1) {
+            return error.ReservedInstruction;
+        }
+        const lanes = if (full) 16 / lane_bytes else 8 / lane_bytes;
+
+        const base_reg = regFromWord(word >> 5);
+        const first_reg = @enumToInt(vectorRegFromWord(word));
+        const start_address = self.readSized(true, base_reg, true);
+        var offset: u64 = 0;
+        var r: usize = 0;
+        while (r < repeat) : (r += 1) {
+            var lane: usize = 0;
+            while (lane < lanes) : (lane += 1) {
+                var field: usize = 0;
+                while (field < fields) : (field += 1) {
+                    const reg = @intToEnum(a64_state.VectorReg, @intCast(u5, (first_reg + r + field) & 31));
+                    const address = start_address +% offset;
+                    if (load) {
+                        var vector = self.state.readVector(reg);
+                        if (!full) {
+                            vector.high = 0;
+                        }
+                        setVectorElement(&vector, lane, lane_bytes, try self.readMemory(address, lane_bytes));
+                        if (!full) {
+                            vector.high = 0;
+                        }
+                        self.state.writeVector(reg, vector);
+                    } else {
+                        const vector = self.state.readVector(reg);
+                        try self.writeMemory(address, lane_bytes, vectorElement(vector, lane, lane_bytes));
+                    }
+                    offset += @intCast(u64, lane_bytes);
+                }
+            }
+        }
+
+        if (writeback) {
+            const offset_reg = regFromWord(word >> 16);
+            const advance = if (offset_reg == .sp) offset else self.readSized(true, offset_reg, false);
+            self.writeSized(true, base_reg, start_address +% advance, true);
+        }
+
         self.state.pc +%= 4;
         return true;
     }
@@ -1834,6 +1936,15 @@ fn vectorByte(value: a64_state.VectorValue, index: usize) u8 {
     return @intCast(u8, (word >> shift) & 0xff);
 }
 
+fn vectorElement(value: a64_state.VectorValue, index: usize, bytes: usize) u64 {
+    var result: u64 = 0;
+    var byte_index: usize = 0;
+    while (byte_index < bytes) : (byte_index += 1) {
+        result |= @as(u64, vectorByte(value, index * bytes + byte_index)) << @intCast(u6, byte_index * 8);
+    }
+    return result;
+}
+
 fn setVectorByte(value: *a64_state.VectorValue, index: usize, byte: u8) void {
     const shift = @intCast(u6, (index & 7) * 8);
     const mask = @as(u64, 0xff) << shift;
@@ -1842,6 +1953,14 @@ fn setVectorByte(value: *a64_state.VectorValue, index: usize, byte: u8) void {
         value.low = (value.low & ~mask) | shifted;
     } else {
         value.high = (value.high & ~mask) | shifted;
+    }
+}
+
+fn setVectorElement(value: *a64_state.VectorValue, index: usize, bytes: usize, element: u64) void {
+    var byte_index: usize = 0;
+    while (byte_index < bytes) : (byte_index += 1) {
+        const byte = @intCast(u8, (element >> @intCast(u6, byte_index * 8)) & 0xff);
+        setVectorByte(value, index * bytes + byte_index, byte);
     }
 }
 
