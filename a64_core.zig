7a2edca1ccc5ca77cf2f701d6875d4dc463e7a60
Connor <homes.hammer-73@icloud.com>
2021-12-25 07:55:41 -0500

    Implement scalar float negation

--- a64_core.zig
+++ a64_core.zig
@@ -1281,7 +1281,8 @@ pub const Core64 = struct {
     }
 
     fn runFloatUnary(self: *Core64, word: u32) Core64Error!bool {
-        if ((word & 0xff3ffc00) != 0x1e20c000) {
+        const masked = word & 0xff3ffc00;
+        if (masked != 0x1e20c000 and masked != 0x1e214000) {
             return false;
         }
 
@@ -1291,7 +1292,10 @@ pub const Core64 = struct {
         }
 
         const source = self.state.readVector(vectorRegFromWord(word >> 5)).low;
-        const result = if (mode == 1) source & 0x7fffffffffffffff else @as(u64, @intCast(u32, source) & 0x7fffffff);
+        const result = if (mode == 1)
+            if (masked == 0x1e20c000) source & 0x7fffffffffffffff else source ^ 0x8000000000000000
+        else
+            @as(u64, if (masked == 0x1e20c000) @intCast(u32, source) & 0x7fffffff else @intCast(u32, source) ^ 0x80000000);
         self.state.writeVector(vectorRegFromWord(word), a64_state.VectorValue{ .low = result, .high = 0 });
         self.state.pc +%= 4;
         return true;
