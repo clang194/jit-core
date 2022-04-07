cfed2046c45cd72df05e9a24dfd8652d41ddac6a
Connor <homes.hammer-73@icloud.com>
2022-04-07 07:44:49 -0400

    Implement scalar equality compare

--- a64_vector_math_flow.zig
+++ a64_vector_math_flow.zig
@@ -203,7 +203,7 @@ pub const Core64Methods = struct {
 
     pub fn runScalarVectorArithmetic(self: *Core64, word: u32) Core64Error!bool {
         const masked = word & 0xff20fc00;
-        if (masked != 0x5e203400 and masked != 0x5e203c00 and masked != 0x5e208400 and masked != 0x7e203400 and masked != 0x7e203c00 and masked != 0x7e208400) {
+        if (masked != 0x5e203400 and masked != 0x5e203c00 and masked != 0x5e208400 and masked != 0x7e203400 and masked != 0x7e203c00 and masked != 0x7e208400 and masked != 0x7e208c00) {
             return false;
         }
 
@@ -225,6 +225,8 @@ pub const Core64Methods = struct {
         else if (masked == 0x7e203400 and left > right)
             ~@as(u64, 0)
         else if (masked == 0x7e203c00 and left >= right)
+            ~@as(u64, 0)
+        else if (masked == 0x7e208c00 and left == right)
             ~@as(u64, 0)
         else
             0;
