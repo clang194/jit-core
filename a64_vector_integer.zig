3c585a2dc1e52258da343f2325e53adefd049dae
Connor <homes.hammer-73@icloud.com>
2022-04-30 12:26:13 -0400

    Implement halving vector add

--- a64_vector_integer.zig
+++ a64_vector_integer.zig
@@ -49,6 +49,19 @@ pub fn signedHalvingAddVectorLanes(left: u64, right: u64, lane: u8) u64 {
     return result;
 }
 
+pub fn halvingAddVectorLanes(left: u64, right: u64, lane: u8) u64 {
+    const mask = ones(lane);
+    var result: u64 = 0;
+    var shift: u8 = 0;
+    while (shift < 64) : (shift += lane) {
+        const amount = @intCast(u6, shift);
+        const left_lane = (left >> amount) & mask;
+        const right_lane = (right >> amount) & mask;
+        result |= (((left_lane + right_lane) >> 1) & mask) << amount;
+    }
+    return result;
+}
+
 pub fn differenceUnsignedVectorLanes(left: u64, right: u64, lane: u8) u64 {
     if (lane == 64) {
         return if (left >= right) left - right else right - left;
