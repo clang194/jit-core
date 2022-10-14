a6975484d88825026ac423ee3039cc47a0f28c68
Connor <homes.hammer-73@icloud.com>
2022-10-14 14:06:18 -0400

    Add floating reciprocal estimate helper

--- float_estimate.zig
+++ float_estimate.zig
@@ -20,6 +20,22 @@ fn estimateByte(value: u64) u8 {
     return @intCast(u8, (guess + 1) / 2);
 }
 
+fn reciprocalByte(value: u64) u8 {
+    const adjusted = value * 2 + 1;
+    const reciprocal = (@as(u64, 1) << 19) / adjusted;
+    return @intCast(u8, (reciprocal + 1) / 2);
+}
+
+fn reciprocalOverflows(negative: bool, control: float_control.Control) bool {
+    return switch (control.rounding()) {
+        .nearest => true,
+        .nearest_away => true,
+        .positive => !negative,
+        .negative => negative,
+        .zero => false,
+    };
+}
+
 fn estimateBits(value: float_parts.WideFloatParts, comptime Result: type, comptime fraction_bits: u6, comptime bias: u64, comptime mask: u64) Result {
     const exponent = (-(value.exponent + 1)) >> 1;
     const even = (value.exponent & 1) == 0;
@@ -28,6 +44,84 @@ fn estimateBits(value: float_parts.WideFloatParts, comptime Result: type, compti
     const stored_exponent = @intCast(u64, exponent + @intCast(i32, bias));
     const stored_mantissa = estimate << (fraction_bits - 8);
     return @intCast(Result, (stored_exponent << fraction_bits) | (stored_mantissa & mask));
+}
+
+fn reciprocalBits32(analysis: float_parts.FloatAnalysis, control: float_control.Control, status: *float_status.FloatStatus) float_exception.FloatExceptionError!u32 {
+    if (analysis.parts.exponent < float_format.Binary32.exponent_min - 2) {
+        try float_exception.processFloatException(.overflow, control, status);
+        try float_exception.processFloatException(.inexact, control, status);
+        return if (reciprocalOverflows(analysis.negative, control)) float_format.Binary32.infinity(analysis.negative) else float_format.Binary32.maxNormal(analysis.negative);
+    }
+    if (control.fz() and analysis.parts.exponent >= -float_format.Binary32.exponent_min) {
+        status.setUnderflow(true);
+        return float_format.Binary32.zero(analysis.negative);
+    }
+
+    var estimate = @as(u32, reciprocalByte(bits.shiftRight64(analysis.parts.significand, float_parts.normalized_point - 8))) << @intCast(u5, float_format.Binary32.stored_fraction_bits - 8);
+    var result_exponent = -analysis.parts.exponent;
+    if (result_exponent == float_format.Binary32.exponent_min - 1) {
+        estimate |= float_format.Binary32.hidden_bit;
+        estimate >>= 1;
+    } else if (result_exponent == float_format.Binary32.exponent_min - 2) {
+        estimate |= float_format.Binary32.hidden_bit;
+        estimate >>= 2;
+        result_exponent = 0;
+    }
+
+    const stored_exponent = @intCast(u32, result_exponent + @intCast(i32, float_format.Binary32.exponent_bias));
+    return float_format.Binary32.zero(analysis.negative) | (stored_exponent << @intCast(u5, float_format.Binary32.stored_fraction_bits)) | (estimate & float_format.Binary32.fraction_mask);
+}
+
+fn reciprocalBits64(analysis: float_parts.FloatAnalysis, control: float_control.Control, status: *float_status.FloatStatus) float_exception.FloatExceptionError!u64 {
+    if (analysis.parts.exponent < float_format.Binary64.exponent_min - 2) {
+        try float_exception.processFloatException(.overflow, control, status);
+        try float_exception.processFloatException(.inexact, control, status);
+        return if (reciprocalOverflows(analysis.negative, control)) float_format.Binary64.infinity(analysis.negative) else float_format.Binary64.maxNormal(analysis.negative);
+    }
+    if (control.fz() and analysis.parts.exponent >= -float_format.Binary64.exponent_min) {
+        status.setUnderflow(true);
+        return float_format.Binary64.zero(analysis.negative);
+    }
+
+    var estimate = @as(u64, reciprocalByte(bits.shiftRight64(analysis.parts.significand, float_parts.normalized_point - 8))) << @intCast(u6, float_format.Binary64.stored_fraction_bits - 8);
+    var result_exponent = -analysis.parts.exponent;
+    if (result_exponent == float_format.Binary64.exponent_min - 1) {
+        estimate |= float_format.Binary64.hidden_bit;
+        estimate >>= 1;
+    } else if (result_exponent == float_format.Binary64.exponent_min - 2) {
+        estimate |= float_format.Binary64.hidden_bit;
+        estimate >>= 2;
+        result_exponent = 0;
+    }
+
+    const stored_exponent = @intCast(u64, result_exponent + @intCast(i32, float_format.Binary64.exponent_bias));
+    return float_format.Binary64.zero(analysis.negative) | (stored_exponent << @intCast(u6, float_format.Binary64.stored_fraction_bits)) | (estimate & float_format.Binary64.fraction_mask);
+}
+
+pub fn reciprocalEstimate32(value: u32, control: float_control.Control, status: *float_status.FloatStatus) float_exception.FloatExceptionError!u32 {
+    const analysis = try float_parts.splitFloat32(value, control, status);
+    switch (analysis.kind) {
+        .quiet_nan, .signaling_nan => return try a64_float_nan.processNan32(control, value, status),
+        .zero => {
+            try float_exception.processFloatException(.divide_by_zero, control, status);
+            return float_format.Binary32.infinity(analysis.negative);
+        },
+        .infinity => return float_format.Binary32.zero(analysis.negative),
+        .finite => return try reciprocalBits32(analysis, control, status),
+    }
+}
+
+pub fn reciprocalEstimate64(value: u64, control: float_control.Control, status: *float_status.FloatStatus) float_exception.FloatExceptionError!u64 {
+    const analysis = try float_parts.splitFloat64(value, control, status);
+    switch (analysis.kind) {
+        .quiet_nan, .signaling_nan => return try a64_float_nan.processNan64(control, value, status),
+        .zero => {
+            try float_exception.processFloatException(.divide_by_zero, control, status);
+            return float_format.Binary64.infinity(analysis.negative);
+        },
+        .infinity => return float_format.Binary64.zero(analysis.negative),
+        .finite => return try reciprocalBits64(analysis, control, status),
+    }
 }
 
 pub fn inverseRootEstimate32(value: u32, control: float_control.Control, status: *float_status.FloatStatus) float_exception.FloatExceptionError!u32 {
