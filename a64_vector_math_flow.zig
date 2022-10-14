44e91016c950edb571f7829600ae922f4363566f
Connor <homes.hammer-73@icloud.com>
2022-10-14 15:28:46 -0400

    Add vector floating minimum

--- a64_vector_math_flow.zig
+++ a64_vector_math_flow.zig
@@ -81,7 +81,7 @@ pub const Core64Methods = struct {
 
     pub fn runVectorFloatBinary(self: *Core64, word: u32) Core64Error!bool {
         const masked = word & 0xbfa0fc00;
-        if (masked != 0x0e20d400 and masked != 0x0ea0d400 and masked != 0x0e20e400 and masked != 0x2e20d400 and masked != 0x2e20dc00 and masked != 0x2e20e400 and masked != 0x2e20ec00 and masked != 0x2e20fc00 and masked != 0x2ea0d400 and masked != 0x2ea0e400 and masked != 0x2ea0ec00) {
+        if (masked != 0x0e20d400 and masked != 0x0ea0d400 and masked != 0x0e20e400 and masked != 0x0ea0f400 and masked != 0x2e20d400 and masked != 0x2e20dc00 and masked != 0x2e20e400 and masked != 0x2e20ec00 and masked != 0x2e20fc00 and masked != 0x2ea0d400 and masked != 0x2ea0e400 and masked != 0x2ea0ec00) {
             return false;
         }
 
@@ -105,6 +105,8 @@ pub const Core64Methods = struct {
             divideFloatVector(control, nan_mode, double, full, left, right)
         else if (masked == 0x2ea0d400)
             absoluteDifferenceFloatVector(control, nan_mode, double, full, left, right)
+        else if (masked == 0x0ea0f400)
+            minimumFloatVector(control, nan_mode, double, full, left, right)
         else if (masked == 0x0e20e400)
             equalFloatVector(control, double, full, left, right)
         else if (masked == 0x2e20e400)
