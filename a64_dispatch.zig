6bae19fa0451e90609743b5839b4c707876e22f9
Connor <homes.hammer-73@icloud.com>
2022-10-30 00:27:41 -0400

    Add vector signed float conversion

--- a64_dispatch.zig
+++ a64_dispatch.zig
@@ -413,6 +413,13 @@ pub const Core64Methods = struct {
             if (vector_float_negate) {
                 return;
             }
+            const vector_float_signed_integer = self.runVectorFloatToSignedInteger(word) catch |err| {
+                try self.raiseFault(err);
+                return;
+            };
+            if (vector_float_signed_integer) {
+                return;
+            }
             const vector_float = self.runVectorFloatBinary(word) catch |err| {
                 try self.raiseFault(err);
                 return;
