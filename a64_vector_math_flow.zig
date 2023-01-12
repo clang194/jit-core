9a5ee444927ff2ddc8c44b735576b87913b1f011
Connor <homes.hammer-73@icloud.com>
2023-01-12 03:21:26 -0500

    Run scalar saturating doubling multiply high

--- a64_vector_math_flow.zig
+++ a64_vector_math_flow.zig
@@ -825,12 +825,16 @@ pub const Core64Methods = struct {
         const saturating_sub = masked == 0x5e202c00;
         const unsigned_saturating_add = masked == 0x7e200c00;
         const unsigned_saturating_sub = masked == 0x7e202c00;
-        if (!saturating_add and !saturating_sub and !unsigned_saturating_add and !unsigned_saturating_sub and masked != 0x5e203400 and masked != 0x5e203c00 and masked != 0x5e204400 and masked != 0x5e205400 and masked != 0x5e208400 and masked != 0x5e208c00 and masked != 0x7e203400 and masked != 0x7e203c00 and masked != 0x7e204400 and masked != 0x7e205400 and masked != 0x7e208400 and masked != 0x7e208c00) {
+        const saturating_multiply_high = masked == 0x5e20b400;
+        if (!saturating_add and !saturating_sub and !unsigned_saturating_add and !unsigned_saturating_sub and !saturating_multiply_high and masked != 0x5e203400 and masked != 0x5e203c00 and masked != 0x5e204400 and masked != 0x5e205400 and masked != 0x5e208400 and masked != 0x5e208c00 and masked != 0x7e203400 and masked != 0x7e203c00 and masked != 0x7e204400 and masked != 0x7e205400 and masked != 0x7e208400 and masked != 0x7e208c00) {
             return false;
         }
 
         const size = @intCast(u2, (word >> 22) & 3);
-        if (!saturating_add and !saturating_sub and !unsigned_saturating_add and !unsigned_saturating_sub and size != 3) {
+        if (saturating_multiply_high and (size == 0 or size == 3)) {
+            return error.ReservedInstruction;
+        }
+        if (!saturating_add and !saturating_sub and !unsigned_saturating_add and !unsigned_saturating_sub and !saturating_multiply_high and size != 3) {
             return error.ReservedInstruction;
         }
 
@@ -846,6 +850,18 @@ pub const Core64Methods = struct {
                 unsignedSaturatedAdd(width, left, right)
             else
                 unsignedSaturatedSub(width, left, right);
+            if (saturated.saturated) {
+                var status = float_status.FloatStatus.init(self.state.floatStatus());
+                status.setSaturated(true);
+                self.state.writeFloatStatus(status.raw());
+            }
+            self.state.writeVector(vectorRegFromWord(word), a64_state.VectorValue{ .low = saturated.value, .high = 0 });
+            self.state.pc +%= 4;
+            return true;
+        }
+        if (saturating_multiply_high) {
+            const width = @as(u8, 8) << @intCast(u3, size);
+            const saturated = signedSaturatedDoublingMultiplyHigh(width, left, right);
             if (saturated.saturated) {
                 var status = float_status.FloatStatus.init(self.state.floatStatus());
                 status.setSaturated(true);
