0baf6c5fb23e87dd95a21f7b71ade846b35d9bf6
Connor <homes.hammer-73@icloud.com>
2023-01-17 02:14:36 -0500

    Add vector count leading zero helper

--- a64_count_bits.zig
+++ a64_count_bits.zig
@@ -29,6 +29,24 @@ pub fn countLeadingZeroes64(value: u64) u64 {
     return count;
 }
 
+pub fn countLeadingZeroesVectorLanes(value: u64, lane: u8) u64 {
+    const mask = (@as(u64, 1) << @intCast(u6, lane)) - 1;
+    var result: u64 = 0;
+    var shift: u8 = 0;
+    while (shift < 64) : (shift += lane) {
+        const amount = @intCast(u6, shift);
+        const element = (value >> amount) & mask;
+        const count = if (lane == 8)
+            @as(u64, @clz(u8, @intCast(u8, element)))
+        else if (lane == 16)
+            @as(u64, @clz(u16, @intCast(u16, element)))
+        else
+            @as(u64, @clz(u32, @intCast(u32, element)));
+        result |= count << amount;
+    }
+    return result;
+}
+
 pub fn countLeadingSignBits32(value: u32) u32 {
     const folded = if ((value & 0x80000000) != 0) ~value else value;
     return countLeadingZeroes32(folded) - 1;
